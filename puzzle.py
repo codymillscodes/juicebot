74fa36432e2f2b1a7665d400106a535a3833922e
idiotdoomspiral <vzualbandt@gmail.com>
2022-05-04 16:14:48 -0700

    chdir

--- puzzle.py
+++ puzzle.py
@@ -1,11 +1,13 @@
 #prompt_file = open('prompt', 'r+')
+import os
+os.chdir('/waffle')
 
 def set_prompt(prompt):
-    with open('/waffle/prompt', 'w') as prompt_file:
+    with open('prompt', 'w') as prompt_file:
         prompt_file.truncate(0)
         prompt_file.write(prompt)
 def get_prompt():
-    with open('/waffle/prompt') as prompt_file:
+    with open('prompt') as prompt_file:
         prompt = prompt_file.read()
         return prompt
 # def set_judge(judge):

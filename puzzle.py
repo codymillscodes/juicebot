5ac9dc774db5dc9cd5fb77a39ca9c596135847c6
idiotdoomspiral <vzualbandt@gmail.com>
2022-05-04 16:35:06 -0700

    explicit

--- puzzle.py
+++ puzzle.py
@@ -3,9 +3,9 @@ import os
 os.chdir('/waffle')
 
 def set_prompt(prompt):
-    with open('prompt', 'w') as prompt_file:
+    with open('prompt', 'w+') as prompt_file:
         prompt_file.truncate(0)
-        prompt_file.write(prompt)
+        prompt_file.write(str(prompt))
         prompt_file.close()
 def get_prompt():
     with open('prompt') as prompt_file:
